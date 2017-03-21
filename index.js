825358e29c56a6de8273a02c01ecca4156615dc6
Leo <raveoli21@gmail.com>
2017-03-21 04:33:03 -0700

    yelp_test_6

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function getWatson(idNum,message){
 
             var location = "";
             if(callYelpApi) {  //we need to call yelp API
-                if(res.entities[0].entity == "cuisine"){
+                if(res.entities){
                     var searchQuery = res.entities[0].value; 
                     var filter = "restaurants";
                     sendResponse(idNum,"Please enter a location: "); 
