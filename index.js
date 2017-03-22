803068fbf0574abd85f3d2951d8f88eea6d10721
Leo <raveoli21@gmail.com>
2017-03-22 13:38:10 -0700

    bugs

--- index.js
+++ index.js
@@ -165,6 +165,7 @@ function getWatson(idNum,message){
                 searchQuery = res.entities[0].value; 
                 callYelpApi = true;  
                 sendResponse(idNum,"Please enter a location: "); 
+                sendResponse(idNum,location + "@@@@");
             }
             else if (res.entities.length > 0){                           //only entity, check if intent has been passed
                 if(hasIntentAlready){
